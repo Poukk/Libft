bc5d7e4c0800effee655c4b7500b62373edb1dcf
Poukk <alexandrefilho.06@hotmail.com>
2025-04-08 22:16:42 -0300

    fix: removing duplicated isspace

--- stdlib/ft_atoi.c
+++ stdlib/ft_atoi.c
@@ -11,12 +11,6 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-static int	ft_isspace(int c)
-{
-	return (c == ' ' || c == '\f' || c == '\n'
-		|| c == '\r' || c == '\t' || c == '\v');
-}
-
 int	ft_atoi(const char *nptr)
 {
 	int	result;
